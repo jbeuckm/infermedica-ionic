cd490510e42c3aa8be6b51598f33829fa719c16b
Joe Beuckman <joseph.beuckman@usbank.com>
2015-06-03 10:56:49 -0500

    popping a question modal

--- www/js/directives/infermedica-form-directive.js
+++ www/js/directives/infermedica-form-directive.js
@@ -1,5 +1,5 @@
 angular.module('infermedica.directives', ['infermedica.services'])
-    .directive('infermedicaForm', function ($rootScope, $ionicGesture, Infermedica) {
+    .directive('infermedicaForm', function ($rootScope, $ionicGesture, Infermedica, $ionicModal) {
 
         var $scope;
 
@@ -20,10 +20,12 @@ angular.module('infermedica.directives', ['infermedica.services'])
                     }
                 }
 
+                $scope.$watch('user', getDiagnosis, true);
+
                 $scope.$watch('search', function (val) {
 
                     if ($scope.search == "") return;
-                    
+
                     clearTimeout(searchTimeout);
 
                     searchTimeout = setTimeout(lookupSearch, 1000);
@@ -34,30 +36,63 @@ angular.module('infermedica.directives', ['infermedica.services'])
                 $scope.searchResults = [];
 
                 function lookupSearch() {
-                    Infermedica.lookup($scope.search, getSex()).success(function (data, status, headers, config) {
-                        console.log(data, status, headers, config);
-
+                    Infermedica.lookup($scope.search, getSex()).success(function (data, status) {
                         $scope.searchResults = [data];
                     });
                 }
 
                 $scope.evidence = [];
 
-                $scope.addObservation = function (id) {
-                    
+                $scope.addObservation = function (observation) {
+
                     $scope.evidence.push({
-                        "id": id,
+                        "id": observation.id,
+                        "name": observation.name,
                         "choice_id": "present",
-//                        "observed_at": new Date()
+                        //                        "observed_at": new Date()
                     });
-                    
+
                     $scope.search = "";
                     $scope.searchResults = [];
-                    
+
+                    getDiagnosis();
+                }
+
+                function getDiagnosis() {
                     if (!$scope.user) return;
-                    
-                    Infermedica.diagnosis($scope.user.age, getSex(), $scope.evidence).success(function (data, status, headers, config) {
+                    if ($scope.evidence.length == 0) return;
+                    Infermedica.diagnosis($scope.user.age, $scope.user.sex, $scope.evidence).success(function (data, status) {
                         console.log(data);
+
+                        handleQuestion(data.question);
+                    });
+                }
+
+                function handleQuestion(question) {
+
+                    $ionicModal.fromTemplateUrl('templates/infermedica-question-modal.html', {
+                        scope: $scope,
+                        animation: 'slide-in-up'
+                    }).then(function (modal) {
+                        $scope.modal = modal;
+                        $scope.modal.show();
+                    });
+
+
+                    $scope.closeModal = function () {
+                        $scope.modal.hide();
+                    };
+                    //Cleanup the modal when we're done with it!
+                    $scope.$on('$destroy', function () {
+                        $scope.modal.remove();
+                    });
+                    // Execute action on hide modal
+                    $scope.$on('modal.hidden', function () {
+                        // Execute action
+                    });
+                    // Execute action on remove modal
+                    $scope.$on('modal.removed', function () {
+                        // Execute action
                     });
                 }
 
